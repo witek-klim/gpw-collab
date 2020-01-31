b5e03dafbd06e603494a6bad99a695543df38421
Witold <wklimczyk@outlook.com>
2020-01-31 16:34:08 +0100

    data collection

--- import_modules.py
+++ import_modules.py
@@ -1,2 +1,17 @@
 import os
 import matplotlib
+from collectSingleGPW import CollectSingleGPW
+from datetime import datetime
+
+class collectData():
+    """ collect data for specified time regime """
+    def __init__(self, fromDate, toDate = None):
+        """ date in format 'dd-mm-yyyy'  """
+        self.fromDate = fromDate
+        self.toDate = toDate if toDate!= None else datetime.today().strftime('%d-%m-%Y')
+        
+    def findRequiredDays(self):
+        self.days = [] # list of days for which data is to be collected in standard dd-mm-yyyy format
+        
+
+        
