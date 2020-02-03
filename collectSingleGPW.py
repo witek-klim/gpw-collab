27bea7b730887dc194bac842980756e4d9c1d008
Witold <wklimczyk@outlook.com>
2020-02-03 12:06:35 +0100

    commit

--- collectSingleGPW.py
+++ collectSingleGPW.py
@@ -3,37 +3,56 @@ from datetime import datetime
 import pandas as pd
             
 class CollectSingleGPW():
-    def __init__(self, date=None):
+    def __init__(self, date=None, verbose = True):
         """ date in format: 'dd-mm'yyyy'  """
         
         self.date = date if date is not None else datetime.today().strftime('%d-%m-%Y')
-        print(self.date)
         self.url = r'https://www.gpw.pl/archiwum-notowan?fetch=0&type=10&instrument=&date={}&show_x=Poka%C5%BC+wyniki'.format(self.date)
-        print(self.url)
         self.picklename = './{}.pkl'.format(self.date)
+        self.verbose = verbose
         self.runDataCollection()
+
         
-    def collectData(self):    
+    def runDataCollection(self):
+        try:
+            self.data = pd.read_pickle(self.picklename)
+            if self.verbose:
+                print('read data from pickle for date: {}'.format(self.date))
+        except FileNotFoundError:
+            self.collectDataFromWeb()
+            if self.verbose:
+                print('collected data from web for date: {}'.format(self.date))
+
+        if self.checkData():
+            if self.verbose:
+                print('saving to pickle')
+            self.save2Pickle()
+
+    def collectDataFromWeb(self):
+        """  collect data from web  """    
         myfile = requests.get(self.url)
         data = myfile.content.split(b'section')[7].split(b'<td class="left">')
         df = pd.DataFrame({'name': [], 'opening': [], 'maximum':[], 'minimum':[],'closing':[], 'chamge_percent':[]})
         for i in range(1,len(data)):
             temp = data[i].replace(b' ', b'').replace(b'\t', b'').replace(b'&nbsp;',b'').replace(b'</td>\n', b'').replace(b',', b'.').split(b'<tdclass="text-right">')
             name, opening, maximum, minimum, closing, change_percent = temp[0].decode("utf-8") , float(temp[2]), float(temp[3]), float(temp[4]), float(temp[5]), float(temp[6])
-            print(name, opening, maximum, minimum, closing, change_percent)
             dftemp = pd.DataFrame([[name, opening, maximum, minimum, closing, change_percent]],
                                     columns = ['name', 'opening', 'maximum', 'minimum','closing', 'chamge_percent'])
             df = df.append(dftemp)
-        self.df = df
-        df.to_pickle(self.picklename)
-        
-    def runDataCollection(self):
-        try:
-            self.df = pd.read_pickle(self.picklename)
-            print('read data from pickle for date: {}'.format(self.date))
-        except FileNotFoundError:
-            self.collectData()
-            print('collected data from web for date: {}'.format(self.date))
+        self.data = df
     
-       
-a = CollectSingleGPW()
+    def checkData(self):
+        """ check data for correctness """
+        if self.data.shape[0] == 0:
+            if self.verbose:
+                print('data incorrect')
+            return False
+        else:
+            if self.verbose:
+                print('data correct')
+            return True
+
+    def save2Pickle(self):
+        self.data.to_pickle(self.picklename)
+
+
---
+++ run_script.py
@@ -0,0 +1,6 @@
+from collectSingleGPW import CollectSingleGPW as collect
+
+a = collect()
+b = collect('28.01.2020')
+
+
