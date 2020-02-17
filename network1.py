b01c0e7289c589ec40dc4b47becd9fe044fb0f4d
Witold Klimczyk <witoldklimczyk@gmail.com>
2020-02-17 19:55:42 +0100

    new

---
+++ network1.py
@@ -0,0 +1,15 @@
+import tensorflow as tf
+from tensorflow import keras
+
+
+
+class Network1():
+    def __init__(self):
+        print('creating Network1')
+
+
+
+if __name__== "__main":
+    network = Network1()
+
+
