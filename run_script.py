d2b36e66da52e28387a0e904747a5f63c85b74c5
witek-klim <60467190+witek-klim@users.noreply.github.com>
2020-02-21 16:02:55 +0100

    Update run_script.py

--- run_script.py
+++ run_script.py
@@ -3,9 +3,6 @@ functions in this script are used to download data from web for currency and sto
 
 """
 
-
-
-
 import pandas as pd
 import matplotlib.pyplot as plt
 import datetime
@@ -13,7 +10,7 @@ import sys
 print(sys.argv, len(sys.argv))
 
 import time
-import sys
+
 
 from sklearn.preprocessing import MinMaxScaler
 
@@ -45,15 +42,17 @@ class CollectData():
     def check_data(self):
         nullsX = self.X.isnull().sum()
         print(f'null elements in X:\n{nullsX}')
-        
-
+    
     def plot_data(self, dpi = 100, skip = 10):
         """
         skip: use to plot only every skip value from data
+
+        to do:
+            - only works when 'Data' is the last column
         """
         from pandas.plotting import register_matplotlib_converters
         register_matplotlib_converters()
-        i = 1
+        i = 0
         plt.figure(dpi = dpi)
         for name in self.names:
             plt.plot(self.X['Data'][::skip], self.X.values[:,i][::skip], label = name)
@@ -61,14 +60,27 @@ class CollectData():
         plt.grid('major', linewidth = 0.1)
         plt.legend()
         plt.xlabel('time')
-        plt.ylabel('PLN')
         plt.show()
 
+    def find_change(self, period = 1):
+        """
+        think about better name for this function
+        it is supposed to calculate relative change e.g. day to day instead of quoting the absolute value
+        """
+        df = pd.DataFrame(data = (self.X.values[:-period,1:] - self.X.values[period:,1:])/self.X.values[1:,1:], columns = self.names)
+        df['Data'] = self.X['Data']
+        cols = list(df.columns.values)
+        cols = cols[-1:] + cols[:-1]
+        df = df[cols]
+        print(df)
+
+        #df = df.set_index('Data')
+        self.X = df
 
 
 class CollectCurrency(CollectData):
     """
-    data format from stooq: Data,Otwarcie,Najwyzszy,Najnizszy,Zamkniecie
+    data format from stooq: [Data,Otwarcie,Najwyzszy,Najnizszy,Zamkniecie]
     """
 
     def __init__(self, names, quantity = 'Zamkniecie', mastercurr = 'pln', date0 = None, daten = None, nyears = 10):
@@ -88,7 +100,7 @@ class CollectCurrency(CollectData):
         i = 0
         data = [None] * len(self.names)
         for name in self.names:
-            data[i] = pd.read_csv(f'https://stooq.pl/q/d/l/?s={name}{self.mastercurr}&d1={self.date0_str}&d2={self.daten_str}&i=d')[['Data', quantity]].rename(columns={quantity: name})
+            data[i] = pd.read_csv(f'https://stooq.pl/q/d/l/?s={name}{self.mastercurr}&d1={self.date0_str}&d2={self.daten_str}&i=d')[['Data', self.quantity]].rename(columns={self.quantity: name})
             j=0
             for d in data[i]['Data']:
                 data[i]['Data'][j] = datetime.datetime.strptime(d, '%Y-%m-%d')
@@ -100,16 +112,43 @@ class CollectCurrency(CollectData):
 
 
 
-curr = CollectCurrency(names = ['chf', 'usd','eur', 'jpy', 'aud', 'gbp'], nyears = 3)
-#curr.check_data()
-curr.plot_data(dpi = 120)
 
+#curr = CollectCurrency()
+#print(curr[0:2]['Zamkniecie'])
+class CollecStock(CollectData):
+    def collect_stock():
+        names = ['cdr', 'dnp']
+        columns = ['Data','Zamkniecie']
+        date0 = datetime.datetime(2015,1,1)
+        data = [None]*len(names)
+        i=0
+        for name in names:
+            temp = pd.read_csv(f'https://stooq.pl/q/d/l/?s={name}&i=d')[columns]
+            j=0
+            for date in temp['Data']:
+                temp['Data'][j] = datetime.datetime.strptime(date, '%Y-%m-%d')
+                j += 1
+            temp = temp.loc[data[i]['Data'] > date0]
+            print(f'is null {temp.isnull().sum()}')
+            print(f'number of days {len(temp.index)}')
+            data[i] = temp
+            i+=1
+
+
+
+curr = CollectCurrency(names = ['chf', 'usd','eur', 'jpy', 'aud', 'gbp'], nyears = 5)
+curr.find_change()
+print(type(curr.X['Data'][0]))
+curr.plot_data(dpi = 120, skip = 10)
 
 from scipy.stats import pearsonr
 X = curr.X
-
 import seaborn as sns
 plt.figure()
-cor = X.corr()
+cor = curr.X.corr()
 sns.heatmap(cor, annot=True, cmap=plt.cm.Reds)
 plt.show()
+
+#curr.check_data()
+
+
